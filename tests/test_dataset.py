d41a3b8df7eee8a4d18e883de34a3848d030a6d2
Taos Transue <spacechips@rocketmail.com>
2025-06-07 15:27:18 -0600

    fix dataset test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -8,21 +8,21 @@ from g_led.data import data_bfs_preprocess
 
 
 def test_backward_facing_step_old_and_new_datasets_equal(engine):
-    cfg = init_hydra_cfg('conf', ['dataset=Dataset'])
+    cfg = init_hydra_cfg('conf', ['dataset=BackwardFacingStep2D', 'dataset.batch_size_val=17', 'model=TransformerBackwardFacingStep2D'])
     conf.orm.create_all(engine)
     with conf.sa.orm.Session(engine) as db:
         cfg = conf.orm.instantiate_and_insert_config(db, cfg)
-        dl = data_bfs_preprocess.BackwardFacingStep2D(
-            cfg.dataset.data_dir,
-            trajectory_max_lens=dict(train=50, val=50),
-            solution_start_times=dict(train=0, val=0),
-            solution_end_times=dict(train=510, val=510),
-            batch_sizes=dict(train=20, val=20)
-        )
+        dl = data_bfs_preprocess.BackwardFacingStep2D(cfg.dataset)
         dl.prepare_data()
         dl.setup('validate')
-        dset = data_bfs_preprocess.bfs_dataset(cfg.dataset.data_dir)
-        dloader = DataLoader(dataset=dset, batch_size=20, shuffle=False)
+        dset = data_bfs_preprocess.bfs_dataset(
+            cfg.dataset.data_dir,
+            trajec_max_len=cfg.dataset.trajectory_time_step_count_val,
+            start_n=cfg.dataset.time_step_count_train,
+            n_span=cfg.dataset.time_step_count_val
+        )
+        assert (dset.solution.unfold(0, cfg.dataset.trajectory_time_step_count_val, 1).permute(0, 4, 1, 2, 3) == dl.val).all()
+        dloader = DataLoader(dataset=dset, batch_size=cfg.dataset.batch_size_val, shuffle=False)
         for i, (batch_original, batch) in enumerate(zip(dloader, dl.val_dataloader(shuffle=False))):
             print(i)
-            assert (batch_original == batch[0]['val']).all()
+            assert (batch_original == batch[0]['val'][:batch_original.shape[0]]).all()
