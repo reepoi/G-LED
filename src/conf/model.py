0e2c617ce8cfc5dde7eded0f5cbff3130288335a
Taos Transue <spacechips@rocketmail.com>
2025-06-07 16:42:09 -0600

    fix config

--- src/conf/model.py
+++ src/conf/model.py
@@ -22,6 +22,8 @@ class Transformer(Trainable):
     attention_head_count: int = orm.make_field(orm.ColumnRequired(sa.Integer), default=0)
     time_step_window_size: int = orm.make_field(orm.ColumnRequired(sa.Integer), default=0)
     layer_norm_epsilon: float = orm.make_field(orm.ColumnRequired(sa.Double), default=1e-5)
-    weight_init_std: float = orm.make_field(orm.ColumnRequired(sa.Double), default=.2)
+    weight_init_std: float = orm.make_field(orm.ColumnRequired(sa.Double), default=.02)
     output_hidden_states: bool = orm.make_field(orm.ColumnRequired(sa.Boolean), default=True)
     output_attentions: bool = orm.make_field(orm.ColumnRequired(sa.Boolean), default=True)
+
+    march_tolerance: float = orm.make_field(orm.ColumnRequired(sa.Double), default=.01)
